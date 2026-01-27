f19ab19ccf7b804e639805464df9e78dc4990e61
Leandro Galbarino <leandrokise753@gmail.com>
2026-01-27 20:49:48 -0300

    Utilizando docker.

--- src/nail_designer/settings.py
+++ src/nail_designer/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = "nail_designer.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
